2a09dd82f9e49ed8fe28067e8800c2a6735ff43b
JayasriV3 <97914906+JayasriV3@users.noreply.github.com>
2023-11-11 12:51:00 +0100

    Basics of JAVA

---
+++ ArithematicOperators.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class ArithematicOperators {
+public static void main(String[] arg) {
+	int x = 1;
+	int y = 2;
+	int z = x+y;
+	int a = x-y;
+	int b = x*y;
+	int c = x/y;
+	int d = x%y;
+	
+	System.out.println("the arithematic operators are the following :");
+	System.out.println("addition " + z);
+	System.out.println("subtraction " + a);
+	System.out.println("multiplication " + b);
+	System.out.println("division " + c);
+	System.out.println("modulus " + d);
+	
+
+}
+}
